fc6f7081ceec3de0d00ce42243448aacf3c070a6
Abdullah Amjad <Abdullah.amjad@workabot.ai>
2026-06-16 03:25:38 -0700

    feat : Read Contacts Added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
 
 const API_TOKEN = process.env.API_KEY;
 
@@ -9,6 +14,37 @@ const headers = {
 };
 
 const BASE_URL = "https://api.hubapi.com";
+
+// Read Contacts 
+
+app.get("/contacts", async (req, res) => {
+  try {
+
+    const response =
+      await axios.get(
+        `${BASE_URL}/crm/v3/objects/contacts`,
+        {
+          headers,
+          params: {
+            properties:
+              "email,firstname,lastname,phone",
+          },
+        }
+      );
+
+    res.status(200).json(response.data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: "Failed to retrieve contacts",
+      error:
+        error.response?.data ||
+        error.message,
+    });
+
+  }
+});
 
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
